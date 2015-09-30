1f52e98708193da832815bc819791d7f1e4eb487
ianngiaw <ian@u.nus.edu>
2015-09-30 19:53:20 +0800

    put validations under validate in useraccount

--- app/models/useraccount.js
+++ app/models/useraccount.js
@@ -3,25 +3,33 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('UserAccount', {
     profileId: {
       type: DataTypes.STRING,
-      notNull: true,
-      notEmpty: true
+      validate: {
+        notNull: true,
+        notEmpty: true
+      },
     },
     hashedId: {
       type: DataTypes.STRING(32),
-      notNull: true,
-      notEmpty: true,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      },
       primaryKey: true
     },
     gender: {
       type: DataTypes.STRING(6),
-      notNull: true,
-      notEmpty: true,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      },
       isIn: [['male', 'female']]
     },
     accessToken: {
       type: DataTypes.STRING,
-      notNull: true,
-      notEmpty: true
+      validate: {
+        notNull: true,
+        notEmpty: true
+      },
     },
     isRegistered: {
       type: DataTypes.BOOLEAN,
