111dd7a444ee5df78d3a419e19a2faac5a211bc3
ianngiaw <ian@u.nus.edu>
2015-09-30 19:54:34 +0800

    added letter model #11

---
+++ app/models/letter.js
@@ -0,0 +1,39 @@
+// Letter schema
+module.exports = function(sequelize, DataTypes) {
+  var Letter = sequelize.define('Letter', {
+    sender: {
+      type: DataTypes.STRING(32),
+      validate: {
+        notNull: true,
+        notEmpty: true
+      },
+      references: {
+        model: 'UserAccounts',
+        key: 'hashedId'
+      }
+    },
+    recipient: {
+      type: DataTypes.STRING(32),
+      validate: {
+        notNull: true,
+        notEmpty: true
+      },
+      references: {
+        model: 'UserAccounts',
+        key: 'hashedId'
+      }
+    },
+    letter: {
+      type: DataTypes.TEXT,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      },
+    }
+  }, {
+    timestamps: true, // sets createdAt and updatedAt
+    paranoid: false, // disables soft deletion
+  });
+
+  return Letter;
+};
