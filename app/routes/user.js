c193ded6c16da34b678775bc3bd3040092b520f2
Bili Xu <xubili93@gmail.com>
2015-10-01 23:47:04 +0800

    Remove tabs and add newline to user route

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,6 +2,7 @@ var userController = require('../controllers/user');
 var tokenController = require('../controllers/token');
 
 exports.init = function(app) {
-	app.get('/user/auth', userController.authenticate);
-	app.get('/user/check', tokenController.verifyToken, userController.check);
-}
+  app.get('/user/auth', userController.authenticate);
+  app.get('/user/check', tokenController.verifyToken, userController.check);
+};
+
