ed34b130b02edc86826ccc6321bece282ee37f1d
ianngiaw <ian@u.nus.edu>
2015-10-02 21:46:39 +0800

    added authentication check to socket io

--- app/sockets/socket.js
+++ app/sockets/socket.js
@@ -1,12 +1,15 @@
 var socketIo = require('socket.io');
+var cookieParser = require('socket.io-cookie');
+var db = require('../../config/sequelize');
+var auth = require('./auth');
 
 exports.init = function(server) {
   var io = socketIo(server);
 
-  io.use(socketJwt.authorize({
-    secret: 'testkey',
-    handshake: true
-  }));
+  // Parses the cookies from client
+  io.use(cookieParser);
+  // Checks if the socket is from a valid user
+  io.use(auth.authenticate);
 
-
-}
+  
+};
