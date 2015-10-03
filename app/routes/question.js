37092a95aaa113e68d22c304628c988f77e067c6
ianngiaw <ian@u.nus.edu>
2015-10-03 16:10:36 +0800

    amended route to reflect that the questions retrieved/posted are user's own

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,6 +2,6 @@ var userController = require('../controllers/user');
 var questionController = require('../controllers/question');
 
 exports.init = function(app) {
-  app.get('/question', userController.requireAuthentication, questionController.getQuestion);
-  app.post('/question', userController.requireAuthentication, questionController.postQuestion);
+  app.get('/question/self', userController.requireAuthentication, questionController.getQuestion);
+  app.post('/question/self', userController.requireAuthentication, questionController.postQuestion);
 }
