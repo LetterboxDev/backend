0e1cc624ebb1a127a41ca509cb22f77478473095
Lei Mingyu <uygnim.iel@gmail.com>
2015-10-03 23:37:27 +0800

    fetch correct result from response array

--- app/controllers/survey.js
+++ app/controllers/survey.js
@@ -29,15 +29,19 @@ exports.getOnboarding = function(req, res) {
 };
 
 exports.postResponses = function(req, res) {
-  for (response in req.body.responses) {
-    for (choice in response.choices) {
+  // for...in gets the key, not the value
+  for (var i = 0; i < req.body.responses.length; i ++) {
+    var response = req.body.responses[i];
+    for (var j = 0; j < response.choices.length; j ++) {
+      var choice = response.choices[j];
       db.SurveyUserAnswer.create({
         user: req.user.hashedId,
         SurveyQuestionId: response.questionId,
         SurveyChoiceId: choice.choiceId
-      })
+      });
     }
   }
+
   return res.status(200).send({status: 'responses posted'});
 };
 
