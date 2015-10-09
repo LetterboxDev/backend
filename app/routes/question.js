b212a03fae226bd30f271c5877373c3262817f47
ianngiaw <ian@u.nus.edu>
2015-10-09 22:21:33 +0800

    updated PUT /questions API to set questions and answers

--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,5 @@ var questionController = require('../controllers/question');
 
 exports.init = function(app) {
   app.get('/questions', userController.requireAuthentication, questionController.getRandomQuestions);
-  app.put('/questions', userController.requireAuthentication, questionController.putWyrSet);
+  app.put('/questions', userController.requireAuthentication, questionController.putUserWyrQuestions);
 }
