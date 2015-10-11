dfcd1d8c830de9a841344aa6314b1456fa8381ad
ianngiaw <ian@u.nus.edu>
2015-10-11 12:46:22 +0800

    added new methods to user routes

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,10 +1,12 @@
 var userController = require('../controllers/user');
 
 exports.init = function(app) {
-  app.get('/auth', userController.checkFacebookTokenParam, userController.validateFacebookToken, userController.extendFacebookToken, userController.storeUserData);
+  app.get('/auth', userController.checkFacebookTokenParam, userController.validateFacebookToken, userController.getMediumProfilePicture, userController.extendFacebookToken, userController.storeUserData);
 
   app.get('/user/self', userController.requireAuthentication, userController.getSelf);
   app.get('/user/id/:hashedId', userController.requireAuthentication, userController.getOtherUser);
+  app.put('/user/location', userController.requireAuthentication, userController.updateLocation);
+  app.put('/user/bio', userController.requireAuthentication, userController.updateBio);
   
   app.param('hashedId', userController.getUser);
 };
