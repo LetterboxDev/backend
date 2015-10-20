4de1cd895ddff90cd8d5d224027d785056de9a18
ianngiaw <ian@u.nus.edu>
2015-10-20 21:04:31 +0800

    Added methods to send a push notifications to the user

--- app/sockets/notifier.js
+++ app/sockets/notifier.js
@@ -1,6 +1,47 @@
 var io = require('../../config/socketio');
+var logger = require('../../config/logger');
 var connectionsCounter = require('./countconnections');
 var db = require('../../config/sequelize');
+var https = require('https');
+
+function sendPushNotification(hashedId, message) {
+  db.UserAccount.findOne({
+    where: {
+      hashedId: hashedId
+    }
+  }).then(function(user) {
+    logger.info('Sending push notification to ' + hashedId + ': ' + message);
+    var notification = {
+      'tokens': [user.pushToken],
+      'notification': {
+        'alert': message
+      }
+    };
+    var data = JSON.stringify(notification);
+    var secret = new Buffer(process.env.IONIC_PRIVATE_KEY).toString('base64').replace(/\n/g, '') + ':';
+    var options = {
+      hostname: 'push.ionic.io',
+      port: 443,
+      path: '/api/v1/push',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+        'X-Ionic-Application-Id': '64f32017',
+        'Authorization': 'Basic ' + secret
+      }
+    };
+    var post = https.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        logger.info('Response: ' + chunk);
+      });
+    });
+
+    post.write(data);
+    post.end();
+  });
+}
 
 exports.notifyOfLetter = function(recipient, letterHash) {
   db.Letter.findOne({
@@ -20,6 +61,7 @@ exports.notifyOfLetter = function(recipient, letterHash) {
       io.to(recipient).emit('letter', letter);      
     } else {
       // send notifications
+      sendPushNotification(recipient, "You've received a letter!");
     }
   });
 };
@@ -29,6 +71,7 @@ exports.notifyOfRoom = function(letterSender, room, approver) {
     io.to(letterSender).emit('roomCreated', {room: room, approverName: approver.firstName});
   } else {
     // send notification
+    sendPushNotification(letterSender, approver.firstName + " just started a chat with you!");
   }
 };
 
@@ -39,5 +82,6 @@ exports.notifyOfMessage = function(senderName, message) {
     io.to(message.recipient).emit('roomMessage', res);
   } else {
     // send notification of message from senderName
+    sendPushNotification(letterSender, senderName + ": " + message.content);
   }
 };
