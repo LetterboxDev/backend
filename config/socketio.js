53e368ed8a82568411e8c14d96408d50a6a96753
ianngiaw <ian@u.nus.edu>
2015-10-26 19:17:15 +0800

    Use https for express deployment

--- config/socketio.js
+++ config/socketio.js
@@ -1,11 +1,15 @@
-var http = require('http');
+var https = require('https');
 var logger = require('./logger');
 var socketIo = require('socket.io');
 var socketJwt = require('socketio-jwt');
 var app = require('./express');
+var privateKey  = fs.readFileSync('/etc/nginx/ssl/letterbox-key.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/nginx/ssl/getletterbox_com.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 
 // Server
-var server = http.createServer(app).listen(app.get('port'), function(){
+var server = https.createServer(credentials, app).listen(app.get('port'), function(){
   logger.info("Letterbox backend listening on port " + app.get('port'));
 });
 var io = socketIo(server);
