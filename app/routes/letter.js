2f43c24c5bd00f5883046d0b3930b43bed64061c
Lei Mingyu <uygnim.iel@gmail.com>
2015-10-31 02:20:54 +0800

    add new route for getting letter from hashId

--- app/routes/letter.js
+++ app/routes/letter.js
@@ -3,10 +3,12 @@ var letterController = require('../controllers/letter');
 
 exports.init = function(app) {
   app.get('/letters', userController.requireAuthentication, letterController.getLetters);
+  app.get('/letters/sender/:userHash', userController.requireAuthentication, letterController.getLetterFromSender);
   app.post('/letters', userController.requireAuthentication, letterController.getRecipient, letterController.checkLetterHashExists, letterController.createLetter, letterController.checkPerfectMatch, letterController.sendLetter);
   app.post('/letters/:letterHash', userController.requireAuthentication, letterController.approveLetter);
   app.delete('/letters/:letterHash', userController.requireAuthentication, letterController.rejectLetter);
   app.put('/letters/:letterHash', userController.requireAuthentication, letterController.readLetter);
 
   app.param('letterHash', letterController.getLetter);
+  app.param('userHash', userController.getUser);
 }
